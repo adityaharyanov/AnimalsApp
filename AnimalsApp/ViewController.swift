82d737bebf8ba31a2c39b4426fa3c9ce523c84cf
Your Nameas <you@example.com>
2024-07-18 09:09:52 +0700

    adjust `SceneDelegate` & `ViewController` for testing purpose

--- AnimalsApp/ViewController.swift
+++ AnimalsApp/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import RxCocoa
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    var param: AnimalApiRepository!
+    
+    convenience init(param: AnimalApiRepository) {
+        self.init()
+        self.param = param;
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        param.getAnimals(by: "fox")
+//            .observe(on: MainScheduler.instance)
+            .subscribe(
+                onNext: { newData in
+                    print(newData)
+                },
+                onError: { err in
+                    print(err.localizedDescription)
+                }, onCompleted: {
+                   print("Complete")
+                }
+            )
     }
-
 
 }
 
